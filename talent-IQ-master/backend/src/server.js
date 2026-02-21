79fd3bb4367a77f46a2955852b09db79e5de0cfc
prashant <prashantsaraswat48@gmail.com>
2026-02-21 10:59:32 +0530

    Update CORS for production

--- talent-IQ-master/backend/src/server.js
+++ talent-IQ-master/backend/src/server.js
@@ -18,7 +18,13 @@ const __dirname = path.resolve();
 // middleware
 app.use(express.json());
 // credentials:true meaning?? => server allows a browser to include cookies on request
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cors({
+  origin: [
+    ENV.CLIENT_URL,
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
