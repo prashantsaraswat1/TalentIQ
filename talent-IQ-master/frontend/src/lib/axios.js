43215e3364f275725b2cf924bc838cdb1ac1787b
prashant <prashantsaraswat48@gmail.com>
2026-02-22 00:38:49 +0530

    Testing API URL

--- talent-IQ-master/frontend/src/lib/axios.js
+++ talent-IQ-master/frontend/src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("PRODUCTION API URL:", import.meta.env.VITE_API_URL);
+
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
